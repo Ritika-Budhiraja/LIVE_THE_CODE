3800272c2a7cadfaef95a428bbd90bc1fbaad939
Archit Jain <123106614+ArchitJ6@users.noreply.github.com>
2023-09-08 14:53:35 +0530

    Add files via upload

---
+++ src/pages/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+  };
+
+  return (
+    <>
+      <div className="login-div">
+        <div className="login">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <input
+                placeholder="Email"
+                className="input-field"
+                type="email"
+                required
+              />
+              <label htmlFor="input-field" className="input-label">
+                Email
+              </label>
+              <span className="input-highlight"></span>
+            </div>
+            <div className="input-container">
+              <input
+                type="password"
+                className="input-field"
+                placeholder="Password"
+                required
+              />
+              <label htmlFor="input-field" className="input-label">
+                Password
+              </label>
+              <span className="input-highlight"></span>
+            </div>
+            <button>Submit</button>
+          </form>
+          {err && (
+            <span className="errorMsg">Incorrect Email or Password!</span>
+          )}
+          <div className="already">
+            <p>
+              Don't have an account? <Link to="/signup">Register</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="colorSVG"></div>
+      <div className="colorSVG2 colorSVG2-login"></div>
+    </>
+  );
+};
+
+export default Login;
